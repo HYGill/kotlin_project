d3d44bf0b8722ca291bd4354fc69cc942c1923df
= <slslalal12@gmail.com>
2022-11-06 15:21:19 +0900

    init project

---
+++ src/main/kotlin/com/example/structure/StructureApplication.kt
@@ -0,0 +1,11 @@
+package com.example.structure
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StructureApplication
+
+fun main(args: Array<String>) {
+	runApplication<StructureApplication>(*args)
+}
